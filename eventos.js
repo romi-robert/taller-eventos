280b2d1fe7733e4433f0f43e322751f20e21f456
romi-robert <rominarobert09@hotmail.com>
2023-08-13 19:41:58 -0300

    Agregue alerta Hola! soy el div

---
+++ eventos.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const button = document.getElementById("boton");
+
+    boton.addEventListener("click", function () {
+        alert("Hola! Soy el div")
+    });
+});
+
